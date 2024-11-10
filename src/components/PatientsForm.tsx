605e065e07a77f7a6413cdff25c38a5c8b061ddf
miguelreal90 <miguelreal90@gmail.com>
2024-11-10 12:16:18 -0400

    usando un componente reutilizable, chidren y la libreria de react hook form para validacion de formularios

--- src/components/PatientsForm.tsx
+++ src/components/PatientsForm.tsx
@@ -1,6 +1,13 @@
+import { useForm } from "react-hook-form"
+import Error from './Error';
 type Props = {}
 
 export default function PatientsForm({}: Props) {
+    const {register,handleSubmit,formState:{errors}}=useForm()
+    const registerPatient = ()=>{
+        console.log('nuevo paciente');
+        
+    }
     return (
 <div className="md:w-1/2 lg:w-2/5 mx-5">
         <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -13,6 +20,7 @@ export default function PatientsForm({}: Props) {
         <form 
             className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
             noValidate
+            onSubmit={handleSubmit(registerPatient)}
         >
             <div className="mb-5">
                 <label htmlFor="name" className="text-sm uppercase font-bold">
@@ -23,7 +31,15 @@ export default function PatientsForm({}: Props) {
                     className="w-full p-3  border border-gray-100"  
                     type="text" 
                     placeholder="Nombre del Paciente" 
+                    {...register('name',{
+                        required:'El nombre del paciente es obligatorio'
+                    })}
                 />
+                {errors.name&&(
+                    <Error>{errors.name?.message?.toString()}</Error>
+                )}
+                
+                
             </div>
 
             <div className="mb-5">
@@ -35,7 +51,13 @@ export default function PatientsForm({}: Props) {
                 className="w-full p-3  border border-gray-100"  
                 type="text" 
                 placeholder="Nombre del Propietario" 
+                {...register('caretaker',{
+                    required:'El nombre del Propietario es obligatorio'
+                })}
             />
+            {errors.caretaker&&(
+                    <Error>{errors.caretaker?.message?.toString()}</Error>
+                )}
             </div>
 
             <div className="mb-5">
@@ -47,7 +69,17 @@ export default function PatientsForm({}: Props) {
                 className="w-full p-3  border border-gray-100"  
                 type="email" 
                 placeholder="Email de Registro" 
+                {...register("email", {
+                    required: "El Email es Obligatorio",
+                    pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Email No Válido'
+                    }
+                })} 
             />
+            {errors.email&&(
+                    <Error>{errors.email?.message?.toString()}</Error>
+                )}
             </div>
 
             <div className="mb-5">
@@ -58,7 +90,14 @@ export default function PatientsForm({}: Props) {
                     id="date"
                     className="w-full p-3  border border-gray-100"  
                     type="date" 
+                    {...register('date',{
+                        required:'la fecha es obligatoria'
+                    })}
+                    
                 />
+                {errors.date&&(
+                    <Error>{errors.date?.message?.toString()}</Error>
+                )}
             </div>
             
             <div className="mb-5">
@@ -69,7 +108,14 @@ export default function PatientsForm({}: Props) {
                     id="symptoms"
                     className="w-full p-3  border border-gray-100"  
                     placeholder="Síntomas del paciente" 
+                    {...register('symptoms',{
+                        required:'los sintomas son obligatorios'
+                    })}
                 ></textarea>
+                {errors.symptoms&&(
+                    <Error>{errors.symptoms?.message?.toString()}</Error>
+                )}
+
             </div>
 
             <input
